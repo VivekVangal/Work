41e1f28788c8ee1952af9645e427db281ab17d9a
n0266319 <n0266319@VDDP33P-29CU8T5.lm.lmig.com>
2017-11-01 10:00:35 -0400

    LinkedList commit

--- DataStructures/src/com/my/datastructure/impl/LinkedList.java
+++ DataStructures/src/com/my/datastructure/impl/LinkedList.java
@@ -8,6 +8,7 @@ public class LinkedList<T extends Object> {
 	public void add(T element) {
 		Node<T> nd = new Node<T>();
 		nd.setData(element);
+		System.out.println("Adding: "+element);
 		if (head == null) {
 			head = nd;
 			tail = nd;
@@ -17,38 +18,115 @@ public class LinkedList<T extends Object> {
 		}
 	}
 
-	public void addAfter(T element,T after) {
+	public void addAfter(T element, T after) {
 		Node<T> tmp = head;
 		Node<T> refNode = null;
-		while(tmp.next!=null)
-		{
-			if(tmp.getNext().compareTo(after)==0)
-			{
-				
+		System.out.println("Traversing to all nodes..");
+		while (tmp.getNext() != null) {
+			if (tmp.compareTo(after) == 0) {
+				refNode = tmp;
+				break;
 			}
+			tmp = tmp.getNext();
+		}
+		if (refNode != null) {
+			Node<T> nodeToAdd = new Node<T>();
+			nodeToAdd.setData(element);
+			nodeToAdd.setNext(refNode.getNext());
+			if (refNode == tail) {
+				tail = nodeToAdd;
+			}
+			refNode.setNext(nodeToAdd);
+			System.out.println("Adding: "+element);
+		} else {
+			System.out.println("Unable to find the given element...");
 		}
 	}
 
+	public void deleteFront() {
+		if(head==null)
+		{
+			System.out.println("Underflow...");
+		}
+		Node<T> tmp = head;
+		head = tmp.getNext();
+		if(head == null) {
+			tail = null;
+		}
+		System.out.println("Deleted "+tmp.getData());
+	}
+	
+	public void deleteAfter(T after) {
+		Node<T> tmp = head;
+		Node<T> refNode = null;
+		System.out.println("Traversing to all nodes..");
+		while(tmp.getNext()!=null)
+		{
+			if(tmp.compareTo(after)==0)
+			{
+				refNode = tmp;
+				break;
+			}
+			tmp = tmp.getNext();
+		}
+		if(refNode !=null)
+		{
+			if(refNode==tail)
+			{
+				System.out.println("No element after that tail node to delete...");
+			}
+			Node<T> nodeToDelete = refNode.getNext();
+			refNode.setNext(nodeToDelete.getNext());
+			if(refNode.getNext() == tail)
+			{
+				tail = refNode;
+			}
+			System.out.println("Deleted "+nodeToDelete.getData());
+		}
+		else
+		{
+			System.out.println("Unable to find the given element...");
+		}
+		
+	}
+	
+	public void traverse() {
+		Node<T> tmp = head;
+		while(tmp!=null)
+		{
+			System.out.println(tmp.getData());
+			tmp=tmp.getNext();
+		}
+	}
+	public static void main(String a[]){
+		LinkedList<Integer> sl = new LinkedList<Integer>();
+		sl.add(3);
+		sl.add(32);
+		sl.add(54);
+		sl.add(89);
+		sl.addAfter(76, 54);
+		sl.traverse();
+		sl.deleteFront();
+		sl.deleteAfter(76);
+		sl.traverse();
+		}
+	
 	private static class Node<T> implements Comparable<T> {
 		private T data;
 		private Node<T> next;
 
-		@SuppressWarnings("unused")
 		public Node<T> getNext() {
 			return next;
 		}
 
-		@SuppressWarnings("unused")
 		public void setNext(Node<T> next) {
 			this.next = next;
 		}
 
-		@SuppressWarnings("unused")
 		public T getData() {
 			return data;
 		}
 
-		@SuppressWarnings("unused")
 		public void setData(T data) {
 			this.data = data;
 		}
